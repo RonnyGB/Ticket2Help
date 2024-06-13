941db24c424b3a05d884f5ff7860bb5c3a631256
ANA LOPES <122389148+ANASILVALOPES@users.noreply.github.com>
2024-06-13 21:49:09 +0100

    sphinx
    Documentação

---
+++ source/conf.py
@@ -0,0 +1,36 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'TICKET2HELP'
+copyright = '2024, ANA SILVA, RONALD COSTA, DIOGO REGADAS'
+author = 'ANA SILVA, RONALD COSTA, DIOGO REGADAS'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+language = 'pt'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
+
+# -- Django setup ------------------------------------------------------------
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../../'))  # Ajuste o caminho conforme necessário
+os.environ['DJANGO_SETTINGS_MODULE'] = 'Ticket2Help_P4.settings'
+import django
+django.setup()
+
---
+++ source/intro.rst
@@ -0,0 +1,14 @@
+Introduction
+============
+
+Bem-vindo à documentação do TICKET2HELP. Este documento fornece uma visão geral do projeto e suas funcionalidades principais.
+
+Principais Funcionalidades:
+---------------------------
+- **Criação de Ticket**: Permite aos usuários criar novos tickets com um ID único, data de criação e outros atributos relevantes.
+- **Atualização de Ticket**: Os tickets podem ser atualizados com informações como a data da última alteração, colaborador responsável pela alteração, e estado do ticket.
+- **Gerenciamento de Hardware**: Suporte para tickets específicos de hardware, incluindo informações sobre o equipamento, avarias, descrição da reparação e peças necessárias.
+- **Gerenciamento de Software**: Suporte para tickets específicos de software, incluindo informações sobre o software, descrição da necessidade e descrição da intervenção.
+- **Métodos de Exibição**: Métodos dedicados para retornar uma representação string dos tickets, facilitando a visualização e o acompanhamento dos mesmos.
+
+
